744b023025ae264de83f877625ca425d25b0ea1f
Fedenov <elfeede_n@hotmail.com>
2022-04-22 06:15:19 -0300

    go copy

---
+++ Tarea_1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import statsmodels as st
+import scipy as stast
+import matplotlib.pyplot as plt
+from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
+
+# datos = pd.read_csv("winequality-red.csv")
+
+## datos.head()
